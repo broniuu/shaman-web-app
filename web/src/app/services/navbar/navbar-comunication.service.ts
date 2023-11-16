b1947b012de46872e1ab5a86aef666b7dbe01122
kamil.martyka <kamil.martyka@elplc.com>
2023-11-16 18:05:45 +0100

    Matangular + nabar i obsługa

---
+++ web/src/app/services/navbar/navbar-comunication.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavbarComunicationService {
+  private functionCalledSource = new Subject<void>();
+  private  barVisibleSource = new Subject<void>();
+  private  barInvisibleSource = new Subject<void>();
+  private  filterSource = new Subject<void>();
+
+  private stringSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  functionCalled$ = this.functionCalledSource.asObservable();
+  barVisibility$ = this.barVisibleSource.asObservable();
+  barInVisibility$ = this.barInvisibleSource.asObservable();
+  filterSearch$ = this.filterSource.asObservable();
+  filterFunction(): void {
+    this.filterSource.next();
+  }
+  getSearch(): Observable<string> {
+    return this.stringSubject.asObservable();
+  }
+  setSearch(value: string): void {
+    this.stringSubject.next(value);
+  }
+  callFunction(): void {
+    this.functionCalledSource.next();
+  }
+  ShowBar(): void {
+    this.barVisibleSource.next();
+  }
+  HideBar() {
+    this.barInvisibleSource.next();
+  }
+}
